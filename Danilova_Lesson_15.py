edb1f25e3f32a5fbb96c3dac57e3687bf6dc0b61
Viktoriatoriaaa <133050101+Viktoriatoriaaa@users.noreply.github.com>
2023-05-19 12:17:18 +0300

    Add files via upload

--- Danilova_Lesson_15.py
+++ Danilova_Lesson_15.py
@@ -1,40 +1,38 @@
-function1 = [1,2,3,'a','bc8?']
-function2 = (1,2,3,'a','bc8?',7,8,9)
-function3 = 788
-function4 = '7f88'
-a=0
-d=0
-c=0
-t=0
-b=0
-S=str()
-for m in str(function1):
-    if m.isalpha():
-        a+=1
-    elif m.isdigit():
-        d+=1
+def print_data_type(func):
+     def wrapper(data):
+          data_type = type(data).__name__
+          print(f'Data_type:{data_type}')
+          return func(data)
+     return wrapper
 
-for v in function2:
-    if type(v)==str:
-        S+=v
-c=len(S)
+@print_data_type
+def function(data):
+     if isinstance(data,tuple):
+        len_str = sum(len(str(item)) for item in data if isinstance(item,str))
+        print(f'Общая длина всех строк: {len_str}')
+     elif isinstance(data,list):
+        total_letters = sum(len(str(item)) for item in data if isinstance(item,str))
+        total_numbers = sum(len(str(item)) for  item in data if isinstance(item, int) or isinstance(item,float))
+        print(f'Количество букв: {total_letters}, количество чисел: {total_numbers}')
+     elif isinstance(data,int):
+        odd_digit_count = sum(1 for digit in str(data) if int(digit)%2!=0)
+        print(f'Количество нечетных цифр: {odd_digit_count}')
+     elif isinstance(data,str):
+        letter_count = sum(1 for letter in data if letter.isalpha())
+        print(f'Количество букв: {letter_count}')
+     else:
+        print('Invalid input type. Please provide a tuple, list or integer.')
 
-for j in str(function3):
-     if int(j)%2 != 0:
-          t+=1
+if __name__=='__main__':
+     function([1,2,3,'a','bc8?'])
+     function((1,2,3,'a','bc8?',7,8,9))
+     function(788)
+     function('7fj88')
+     function({1,2,3,4,5})
 
-for i in function4:
-     if i.isalpha():
-          b+=1
-def function1(): print([1,2,3,'a','bc8?'], 'Это список, в котором ',a, 'букв', d, 'чисел')
-def function2(): print((1,2,3,'a','bc8?',7,8,9),'Это кортеж, в котором ',c, 'символов')
-def function3(): print(788,'Это число, в котором ',t, ' нечетных цифр')
-def function4(): print('7f88','Это строка, в которой ',b, 'букв')
 
-def simple_decore(fn):
-     fn()
 
-simple_decore(function1)
-simple_decore(function2)
-simple_decore(function3)
-simple_decore(function4)
+
+
+
+
